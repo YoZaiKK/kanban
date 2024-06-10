605c1bd81cdae3449d64ae6bdc3bb5fe511b8ae5
Guillermo Cardona <memocargo@hotmail.com>
2024-06-10 08:24:46 -0600

    fix card doin weird

--- src/components/BoardsTiles.tsx
+++ src/components/BoardsTiles.tsx
@@ -32,65 +32,64 @@ export const BoardsTiles = ({ boards }: { boards: RoomInfo[] }) => {
 			<div className="my-4 grid grid-cols-1 gap-2">
 				{boards?.length > 0 &&
 					boards.map((board) => (
-						<div key={board.id}>
-							<Card
-								isPressable
-								onPress={() => router.push(`/boards/${board.id}`)}
-								className="rounded-md shadow-md hover:shadow-lg duration-300 transition-shadow ease-in-out"
-							>
-								<CardHeader>
-									<div className="text-lg text-gray-500">
-										Title:{" "}
-										<span className="text-2xl font-bold">
-											{board.metadata.boardName}
-										</span>
-									</div>
-								</CardHeader>
-								{/* <Divider /> */}
-								<CardBody>
-									<span>Description</span>
-									<div className="grid grid-cols-2">
-										<div className="font-normal text-medium text-gray-500">
-											<div className=" text-gray-500">
-												Autor:{" "}
-												<span className="text-primaryColor">
-													{board.metadata.createdBy}
-												</span>
-											</div>
-											<div className="text-gray-500">
-												ID: <span className="text-thirdColor">{board.id}</span>
-											</div>
+						<Card
+							key={board.id}
+							isPressable
+							onPress={() => router.push(`/boards/${board.id}`)}
+							className="rounded-md shadow-md hover:shadow-lg duration-300 transition-shadow ease-in-out"
+						>
+							<CardHeader>
+								<div className="text-lg text-gray-500">
+									Title:{" "}
+									<span className="text-2xl font-bold">
+										{board.metadata.boardName}
+									</span>
+								</div>
+							</CardHeader>
+							{/* <Divider /> */}
+							<CardBody>
+								<span>Description</span>
+								<div className="grid grid-cols-2">
+									<div className="font-normal text-medium text-gray-500">
+										<div className=" text-gray-500">
+											Autor:{" "}
+											<span className="text-primaryColor">
+												{board.metadata.createdBy}
+											</span>
 										</div>
-										<div className="flex justify-end">
-											<div className="grid grid-cols-1">
-												<Chip>User</Chip>
-												<Chip>User</Chip>
-											</div>
+										<div className="text-gray-500">
+											ID: <span className="text-thirdColor">{board.id}</span>
 										</div>
 									</div>
-								</CardBody>
-								<CardFooter className="flex justify-between">
-									<Button
-										className="
+									<div className="flex justify-end">
+										<div className="grid grid-cols-1">
+											<Chip>User</Chip>
+											<Chip>User</Chip>
+										</div>
+									</div>
+								</div>
+							</CardBody>
+							<CardFooter className="flex justify-between">
+								<Button
+									className="
 							 bg-red-500 text-white p-2 rounded-md hover:bg-red-600 duration-300 hover:shadow-lg 
 							"
-										onClick={async () => {
-											await handleDeleteBoard(board.id);
-										}}
-									>
-										<FontAwesomeIcon icon={faTrash} />
-									</Button>
-									<RoomProvider id={board.id} initialPresence={{}}>
-										<div className="absolute bottom-1 right-1">
-											<PresenceAvatars
-												presenceKey="boardId"
-												presenceValue={board.id}
-											/>
-										</div>
-									</RoomProvider>
-								</CardFooter>
-							</Card>
-						</div>
+									onClick={async () => {
+										await handleDeleteBoard(board.id);
+									}}
+								>
+									<FontAwesomeIcon icon={faTrash} />
+								</Button>
+								<RoomProvider id={board.id} initialPresence={{}}>
+									<div className="absolute bottom-1 right-1">
+										<PresenceAvatars
+											presenceKey="boardId"
+											presenceValue={board.id}
+										/>
+									</div>
+								</RoomProvider>
+							</CardFooter>
+						</Card>
 					))}
 			</div>
 		</>
